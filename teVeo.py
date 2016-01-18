4390b1cd34f6ce3a5aae3f9aa34bc4b4fcdbf427
root <root@localhost.localdomain>
2016-01-18 00:30:05 +0000

    Added menu and scan all probes option

--- teVeo.py
+++ teVeo.py
@@ -2,6 +2,14 @@ from scapy.all import *
 
 ap_list = []
 
+banner = '''
+--==TeVeO==-- 
+a stalkers best friend...
+
+Version: %s
+Programador: %s
+''' % ('0.1','Nicotrial')
+
 def PacketHandler(pkt):
 	if pkt.haslayer(Dot11):
 		if pkt.type == 0 and pkt.subtype == 4:
@@ -14,4 +22,34 @@ def PacketHandler(pkt):
 			if pkt.addr2 == "34:4d:f7:38:59:a8":
 				print "--detectado Pat signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
 
-sniff(iface="wlan0",prn = PacketHandler)
+def PacketHandler2(pkt):
+	if pkt.haslayer(Dot11):
+		if pkt.type == 0 and pkt.subtype == 4:
+			print "Probe Detected: %s" %(pkt.addr2)
+
+
+def main():
+	help = '''
+--------------------------------------
+	Remember: Start wifi in monitor mode before using 
+		  or you will not capture anything	
+	Usage:./teVeo.py -s
+	
+	Options:
+		-s: Begin Scan
+		-a: Scan all Probes
+	
+	Ejemplo: ./teVeo.py -s
+	ctrl-c to end..
+--------------------------------------
+	'''
+	if sys.argv[1] == '-s':
+		sniff(iface="wlan0",prn = PacketHandler)
+	if sys.argv[1] == '-a':
+		sniff(iface="wlan0",prn = PacketHandler2)
+	else:
+		print(help)
+
+if __name__ == '__main__':
+	print(banner)
+	main()
