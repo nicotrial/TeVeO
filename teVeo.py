e27d979d662e25fde72d746af01c780951e02938
nicotrial <nicolas.logghe@edu.uah.es>
2016-02-01 22:55:23 +0100

    Update teVeo.py
    small chages so people dont think those are legit macs...

--- teVeo.py
+++ teVeo.py
@@ -13,14 +13,14 @@ Programador: %s
 def PacketHandler(pkt):
 	if pkt.haslayer(Dot11):
 		if pkt.type == 0 and pkt.subtype == 4:
-			if pkt.addr2 == "f8:a9:d0:4f:52:30":
-				print "--detectado Nico signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
-			if pkt.addr2 == "48:a2:2d:76:28:93":
-				print "--detectado Dad signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
-			if pkt.addr2 == "5c:95:ae:e0:8c:da":
-				print "--detectado Jou signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
-			if pkt.addr2 == "34:4d:f7:38:59:a8":
-				print "--detectado Pat signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
+			if pkt.addr2 == "ff:af:df:4f:5f:3f":
+				print "--detectado Persona1 signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
+			if pkt.addr2 == "4f:af:2f:7f:2f:9f":
+				print "--detectado Persona2 signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
+			if pkt.addr2 == "5f:9f:af:ef:8f:df":
+				print "--detectado Persona3 signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
+			if pkt.addr2 == "3f:4f:ff:3f:5f:af":
+				print "--detectado Persona4 signal = %d" %(-(256-ord(pkt.notdecoded[-4:-3])))
 
 def PacketHandler2(pkt):
 	if pkt.haslayer(Dot11):
